43812ef3df0d74ff108ba7579b10ad4991075004
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-04-26 16:49:39 -0700

    Fix docs, make bors depend on docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,13 @@
-using AutoPages, Documenter
+using Documenter
+using AutoPages
+using AutoPages: gather_pages, replace_reverse, transform_file
 
 tutorials_dir = joinpath(@__DIR__, "..", "tutorials") # tutorials directory
 mkpath(tutorials_dir)
 tutorials, tutorials_list = gather_pages(;
   directory=tutorials_dir,
   extension_filter=x->endswith(x, ".jl"),
+  transform_extension=x->replace_reverse(x, ".jl" => ".md"; count=1),
   remove_first_level=true)
 
 #### Let's suppose we've used Literate to generate markdown:
@@ -12,7 +15,7 @@ for tutorial in tutorials_list
     mkpath(joinpath(@__DIR__,"src",dirname(tutorial)))
 end
 for tutorial in tutorials_list
-    title = AutoPages.transform_file(basename(tutorial))
+    title = transform_file(basename(tutorial))
     open(joinpath(@__DIR__,"src",tutorial), "w") do io
     print(io, "# $(title)")
     end
